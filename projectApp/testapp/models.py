dbe93e4570dfac3b4c25a2de1e2acbd8f7f72a35
[username] <yourname@email.com>
2022-10-22 10:46:27 +0300

    提交信息post

---
+++ projectApp/testapp/models.py
@@ -0,0 +1,26 @@
+# Create your models here.
+from django.db import models
+
+
+class Posts(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
